d563c8c4fa18a79144cb5e0d8b656c7836487989
Artem Alupov <50357999+aalupov@users.noreply.github.com>
2019-11-17 00:27:56 +0300

    Update member.php

--- member.php
+++ member.php
@@ -21,7 +21,7 @@ $tariff = 3.7;
 </head>
 <body>
 
-<!--Block to show info of the member-->
+	<!--Block to show info of the member-->
 <?php
 $mysql = new Database();
 $sql = "SELECT name FROM garages WHERE number = " . $id;
@@ -34,9 +34,9 @@ $result = $mysql->Rows();
 	<center>
 		<br> <a class="btn btn-primary btn-lg btn-block" href="index.php"
 			role="button">Home</a> <br> <br>
-<!--Block to show info of the member-->
+		<!--Block to show info of the member-->
 
-<!--Block to add the new payment-->			
+		<!--Block to add the new payment-->			
 										<?php
 
         if ($_POST['year_put'] != 'NULL' && $_POST['quarter_put'] != 'NULL' && ! empty($_POST['payment']) && ! empty($_POST['data_put'])) {
@@ -46,11 +46,16 @@ $result = $mysql->Rows();
             } else {
                 $prepaid_put = 'NULL';
             }
+            if (! empty($_POST['salary_put'])) {
+                $salary_put = $_POST['salary_put'];
+            } else {
+                $salary_put = 'NULL';
+            }
             $year_put = $_POST['year_put'];
             $quarter_put = $_POST['quarter_put'];
             $data_put = $_POST['data_put'];
             $payment = $_POST['payment'];
-            $sql = "SELECT id,quarter,data,paid,prepaid,year FROM data WHERE garage_id = " . $id . "
+            $sql = "SELECT id,quarter,data,paid,prepaid,year,salary FROM data WHERE garage_id = " . $id . "
      AND year = " . $year_put . " AND quarter = " . $quarter_put . " ORDER BY year LIMIT 1";
             $mysql->Query($sql);
             $row = $mysql->Rows()[0];
@@ -69,6 +74,7 @@ $result = $mysql->Rows();
 					<th>Data</th>
 					<th>Paid</th>
 					<th>Prepaid</th>
+					<th>Salary</th>
 					<th>Remove</th>
 				</tr>
 			</thead>
@@ -81,6 +87,7 @@ $result = $mysql->Rows();
 					<td><?php echo $row['data']; ?></td>
 					<td><?php echo $row['paid']; ?></td>
 					<td><?php echo $row['prepaid']; ?></td>
+					<td><?php echo $row['salary']; ?></td>
 					<td><a class="btn btn-primary"
 						href="member.php?id=<?php echo $id; ?>&index_id=<?php echo $row['id']; ?>"
 						role="button" onclick="return confirm('Are you sure?')">Remove</a></td>
@@ -93,8 +100,8 @@ $result = $mysql->Rows();
 
     <?php
             } else {
-                $sql = "INSERT INTO data (id, garage_id, quarter, year, data, paid, prepaid) 
-             VALUES (NULL, " . $id . ", " . $quarter_put . ", " . $year_put . ", " . $data_put . ", " . $payment . ", " . $prepaid_put . ")";
+                $sql = "INSERT INTO data (id, garage_id, quarter, year, data, paid, prepaid, salary) 
+             VALUES (NULL, " . $id . ", " . $quarter_put . ", " . $year_put . ", " . $data_put . ", " . $payment . ", " . $prepaid_put . ", " . $salary_put . ")";
                 $mysql->UpdateDb($sql);
                 ?>
     		<div class="alert alert-success" role="alert">Data have been saved</div>
@@ -102,9 +109,9 @@ $result = $mysql->Rows();
             }
         }
         ?>
-<!--Block to add the new payment-->	
+<!--Block to add the new payment-->
 
-<!--Block to count the payment for electricity-->	        
+		<!--Block to count the payment for electricity-->	        
  <?php
 if (! empty($_POST['data_count'])) {
     $data_count = $_POST['data_count'];
@@ -119,9 +126,9 @@ if (! empty($_POST['data_count'])) {
     <?php
 }
 ?>
-<!--Block to count the payment for electricity-->	
+<!--Block to count the payment for electricity-->
 
-<!--Block to remove the payment for electricity-->	       
+		<!--Block to remove the payment for electricity-->	       
         <?php
         if (isset($_GET['index_id'])) {
             $index_id = $_GET['index_id'];
@@ -135,7 +142,7 @@ if (! empty($_POST['data_count'])) {
         ?>
 <!--Block to remove the payment for electricity-->
 
-<!--Block to get the current data for electricity-->        
+		<!--Block to get the current data for electricity-->
 		<form action="member.php?id=<?php echo $id; ?>" method="post">
 			<div class="input-group mb-3">
 				<div class="input-group-prepend">
@@ -183,6 +190,7 @@ if (isset($_POST['year'])) {
 					<th>Data</th>
 					<th>Paid</th>
 					<th>Prepaid</th>
+					<th>Salary</th>
 					<th>Remove</th>
 				</tr>
 			</thead>
@@ -194,14 +202,14 @@ if (isset($_POST['year'])) {
     if ($_POST['year'] != 'NULL' && $_POST['quarter'] != 'NULL') {
         $year = $_POST['year'];
         $quarter = $_POST['quarter'];
-        $sql = "SELECT id,year,data,paid,prepaid FROM data WHERE garage_id = " . $id . " 
+        $sql = "SELECT id,year,data,paid,prepaid,salary FROM data WHERE garage_id = " . $id . " 
      AND quarter = " . $quarter . " AND year = " . $year . " ORDER BY year";
     } elseif ($_POST['year'] != 'NULL' && $_POST['quarter'] == 'NULL') {
         $year = $_POST['year'];
-        $sql = "SELECT id,year,quarter,data,paid,prepaid FROM data WHERE garage_id = " . $id . "
-     AND year = " . $year . " ORDER BY year";
+        $sql = "SELECT id,year,quarter,data,paid,prepaid,salary FROM data WHERE garage_id = " . $id . "
+     AND year = " . $year . " ORDER BY year,quarter";
     } elseif ($_POST['year'] == 'NULL' && $_POST['quarter'] == 'NULL') {
-        $sql = "SELECT * FROM data WHERE garage_id = " . $id . " ORDER BY year";
+        $sql = "SELECT * FROM data WHERE garage_id = " . $id . " ORDER BY year,quarter";
     }
     $mysql->Query($sql);
     foreach ($mysql->Rows() as $row) {
@@ -214,6 +222,7 @@ if (isset($_POST['year'])) {
         $year = $row['year'];
         $data = $row['data'];
         $paid = $row['paid'];
+        $salary = $row['salary'];
         $prepaid = $row['prepaid'];
 
         ?>
@@ -223,6 +232,7 @@ if (isset($_POST['year'])) {
 					<td><?php echo $data; ?></td>
 					<td><?php echo $paid; ?></td>
 					<td><?php echo $prepaid; ?></td>
+					<td><?php echo $salary; ?></td>
 					<td><a class="btn btn-primary"
 						href="member.php?id=<?php echo $id; ?>&index_id=<?php echo $index_id; ?>"
 						role="button" onclick="return confirm('Are you sure?')">Remove</a></td>
@@ -239,9 +249,9 @@ if (isset($_POST['year'])) {
 }
 
 ?>
-<!--Block to get the current data for electricity-->  
+<!--Block to get the current data for electricity-->
 		<br>
-<!--Form to count for electricity--> 		
+		<!--Form to count for electricity-->
 		<h3>Counting</h3>
 		<br>
 		<form action="member.php?id=<?php echo $id; ?>" method="post">
@@ -255,9 +265,9 @@ if (isset($_POST['year'])) {
 			<br> <input class="btn btn-secondary btn-lg btn-block" type="submit"
 				value="Count">
 		</form>
-<!--Form to count for electricity--> 		
+		<!--Form to count for electricity-->
 		<br>
-<!--Form to add new data for electricity--> 		
+		<!--Form to add new data for electricity-->
 		<h3>Put New Data</h3>
 		<br>
 		<form action="member.php?id=<?php echo $id; ?>" method="post">
@@ -304,11 +314,131 @@ if (isset($_POST['year'])) {
 				<input type="number" class="form-control" aria-label="prepaid_put"
 					name="prepaid_put" aria-describedby="inputGroup-sizing-default">
 			</div>
-
+					<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text" id="inputGroup-sizing-default">Salary</span>
+				</div>
+				<input type="number" class="form-control" aria-label="salary_put"
+					name="salary_put" aria-describedby="inputGroup-sizing-default">
+			</div>	
 			<br> <input class="btn btn-secondary btn-lg btn-block" type="submit"
 				value="Save Data">
 		</form>
-<!--Form to add new data for electricity--> 		
+		<!--Form to add new data for electricity-->
+		<br>
+		<!--Block to get the current data by all members-->
+		<form action="member.php?id=<?php echo $id; ?>" method="post">
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<label class="input-group-text" for="year_all">Year</label>
+				</div>
+				<select class="custom-select" name="year_all" id="year_all">
+					<option selected>NULL</option>
+					<option value="2019">2019</option>
+					<option value="2020">2020</option>
+					<option value="2021">2021</option>
+					<option value="2022">2022</option>
+					<option value="2023">2024</option>
+				</select>
+				<div class="input-group-prepend">
+					<label class="input-group-text" for="quarter_all">Quarter</label>
+				</div>
+				<select class="custom-select" name="quarter_all" id="quarter_all">
+					<option selected>NULL</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+				</select>
+			</div>
+			<br> <input class="btn btn-secondary btn-lg btn-block" type="submit"
+				value="Get Data by Everybody">
+		</form>
+		<br>
+
+<?php
+
+if (isset($_POST['year_all'])) {
+    ?>
+    		<!--Table-->
+		<table class="table table-striped w-auto">
+
+			<!--Table head-->
+			<thead>
+				<tr>
+					<th>Number</th>
+					<th>Name</th>
+					<th>Year</th>
+					<th>Quarter</th>
+					<th>Data</th>
+					<th>Paid</th>
+					<th>Prepaid</th>
+					<th>Salary</th>
+				</tr>
+			</thead>
+			<!--Table head-->
+			<!--Table body-->
+			<tbody>
+	<?php
+
+    if ($_POST['year_all'] != 'NULL' && $_POST['quarter_all'] != 'NULL') {
+        $year = $_POST['year_all'];
+        $quarter = $_POST['quarter_all'];
+        $sql = "SELECT garages.number,garages.name,data.year,data.quarter,data.data,data.paid,data.prepaid,data.salary FROM data, garages 
+          WHERE data.quarter = " . $quarter . " 
+          AND data.year = " . $year . " 
+          AND data.garage_id = garages.number 
+          ORDER BY data.garage_id";
+    } elseif ($_POST['year_all'] != 'NULL' && $_POST['quarter_all'] == 'NULL') {
+        $year = $_POST['year_all'];
+        $sql = "SELECT garages.number,garages.name,data.year,data.quarter,data.data,data.paid,data.prepaid,data.salary FROM data,garages 
+          WHERE data.year = " . $year . " 
+          AND data.garage_id = garages.number 
+          ORDER BY data.garage_id";
+    } elseif ($_POST['year_all'] == 'NULL' && $_POST['quarter_all'] == 'NULL') {
+        $sql = "SELECT garages.number,garages.name,data.year,data.quarter,data.data,data.paid,data.prepaid,data.salary FROM data,garages  
+         WHERE data.garage_id = garages.number
+         ORDER BY data.garage_id";
+    }
+    $mysql->Query($sql);
+    foreach ($mysql->Rows() as $row) {
+        if ($quarter) {
+            $quart = $quarter;
+        } else {
+            $quart = $row['quarter'];
+        }
+        $number = $row['number'];
+        $name = $row['name'];
+        $year = $row['year'];
+        $data = $row['data'];
+        $paid = $row['paid'];
+        $salary = $row['salary'];
+        $prepaid = $row['prepaid'];
+
+        ?>
+				<tr class="table-info">
+					<th scope="row"><?php echo $number; ?></th>
+					<td><?php echo $name; ?></td>
+					<td><?php echo $year; ?></td>
+					<td><?php echo $quart; ?></td>
+					<td><?php echo $data; ?></td>
+					<td><?php echo $paid; ?></td>
+					<td><?php echo $prepaid; ?></td>
+					<td><?php echo $salary; ?></td>
+				</tr>
+		<?php
+    }
+    ?>
+    			</tbody>
+			<!--Table body-->
+		</table>
+		<!--Table-->
+		<br>
+    <?php
+}
+
+?>
+<!--Block to get the current data by all members-->
 		<br>
 	</center>
 </body>
